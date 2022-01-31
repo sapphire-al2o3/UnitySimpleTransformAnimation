05ae73a66b4d7bdc161414717e7b79fd9eddcf27
sapphire-al2o3 <t_telkun@hotmail.com>
2022-01-31 21:52:20 +0900

    Initial commit

---
+++ Assets/SimpleScalingAnimation.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleScalingAnimation : MonoBehaviour
+{
+	[SerializeField]
+	Vector3 start = new Vector3(1.0f, 1.0f, 1.0f);
+
+	[SerializeField]
+	Vector3 end = new Vector3(1.0f, 1.0f, 1.0f);
+
+	[SerializeField]
+	AnimationCurve curve;
+
+	[SerializeField]
+	float duration = 1.0f;
+
+	float _time;
+
+    void Update()
+    {
+		float time = (_time / duration);
+		float t = curve.Evaluate(time);
+		transform.localScale = Vector3.Lerp(start, end, t);
+
+		_time += Time.deltaTime;
+
+		if (_time > duration * 2.0f)
+		{
+			_time -= duration * 2.0f;
+		}
+    }
+}
