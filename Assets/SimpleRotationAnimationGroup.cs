c6a3f3dc6c3d6f8bd2eb66e69a70e657dc923b20
sapphire-al2o3 <t_telkun@hotmail.com>
2022-02-17 22:39:34 +0900

    1つのコンポーネントで回転させるスクリプト追加

---
+++ Assets/SimpleRotationAnimationGroup.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotationAnimationGroup : MonoBehaviour
+{
+	[SerializeField]
+	Vector3 start = new Vector3(0.0f, 0.0f, 0.0f);
+
+	[SerializeField]
+	Vector3 end = new Vector3(0.0f, 0.0f, 0.0f);
+
+	[SerializeField]
+	AnimationCurve curve;
+
+	[SerializeField]
+	float duration = 1.0f;
+
+	[SerializeField]
+	public float[] offsets;
+
+	[SerializeField]
+	public Transform[] transforms;
+
+	float _time;
+
+	void Update()
+	{
+		for (int i = 0; i < transforms.Length; i++)
+		{
+			var trans = transforms[i];
+			float time = _time + (offsets != null ? offsets[i] : 0.0f);
+			if (time > duration * 2.0f)
+			{
+				time -= duration * 2.0f;
+			}
+			float t = curve.Evaluate(time / duration);
+			var euler = Vector3.Lerp(start, end, t);
+			transform.localRotation = Quaternion.Euler(euler);
+		}
+
+		_time += Time.deltaTime;
+
+		if (_time > duration * 2.0f)
+		{
+			_time -= duration * 2.0f;
+		}
+	}
+}
